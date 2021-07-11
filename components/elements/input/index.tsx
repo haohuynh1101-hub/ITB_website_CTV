545439d834145860b318c724b43b79da80b78563
Hao Huynh <haohuynh1101-hub>
2021-07-11 21:21:38 +0700

    feat: fix inputRef null

--- components/elements/input/index.tsx
+++ components/elements/input/index.tsx
@@ -1,36 +1,40 @@
-import classnames from 'classnames';
-import React, { useRef } from 'react';
+import classnames from "classnames";
+import { nanoid } from "nanoid";
+import React, {  useEffect, useRef } from "react";
+import { useState } from "react";
 
-import { Prefix } from './Prefix';
+import { Prefix } from "./Prefix";
 
 const SIZE_MAPS = {
   small: {
-    wrapper: 'input-wrapper-sm',
-    input: 'input-sm',
+    wrapper: "input-wrapper-sm",
+    input: "input-sm",
   },
-  middle: '',
+  middle: "",
   large: {
-    wrapper: 'input-wrapper-lg',
-    input: 'input-lg',
+    wrapper: "input-wrapper-lg",
+    input: "input-lg",
   },
 };
+
 const TYPE_MAPS = {
   primary: {
-    wrapper: 'input-wrapper-primary',
-    input: 'input-primary',
+    wrapper: "input-wrapper-primary",
+    input: "input-primary",
   },
   default: {
-    wrapper: 'input-wrapper-default',
-    input: 'input-default',
+    wrapper: "input-wrapper-default",
+    input: "input-default",
   },
 };
 
 const key = {
-  wrapper: 'wrapper',
-  input: 'input',
+  wrapper: "wrapper",
+  input: "input",
 };
+
 const BORDERED_MAPS = {
-  true: 'border-2',
+  true: "border-2",
 };
 
 export type IProps = {
@@ -39,72 +43,109 @@ export type IProps = {
   prefix?: React.ReactNode;
   size?: keyof typeof SIZE_MAPS;
   type?: keyof typeof TYPE_MAPS;
-  htmlType?: 'password' | 'number' | 'text';
+  htmlType?: "password" | "number" | "text";
   value?: string;
   bordered?: boolean;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
   onPressEnter?: React.KeyboardEventHandler<HTMLInputElement>;
+  onBlur?: React.FocusEventHandler<HTMLInputElement>;
+  onFocus?: React.FocusEventHandler<HTMLInputElement>;
+  disabled?: boolean;
 };
+
 export const Input: React.FC<IProps> = ({
   placeholder,
-  htmlType = 'text',
+  htmlType = "text",
   label,
-  size = 'middle',
-  type = 'default',
+  size = "middle",
+  type = "default",
   onChange,
   value,
   prefix,
   bordered = false,
   onPressEnter,
+  onBlur,
+  onFocus,
+  disabled = false,
 }) => {
-  const inputRef = useRef(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const uuid = useRef<string>();
+
+  useEffect(() => {
+    uuid.current = nanoid();
+  }, []);
 
   function onClickToFocus() {
-    inputRef.current.focus();
+    if(inputRef.current){
+      inputRef.current.focus();
+    }
+
   }
+
   function onKeyPress(e) {
     if (!onPressEnter) return;
     if (!e) e = window.event;
     const keyCode = e.code || e.key;
-    if (keyCode == 'Enter') {
-      if (value == '') {
+    if (keyCode == "Enter") {
+      if (value == "") {
         return;
       }
       onPressEnter(e);
     }
   }
+
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    setIsFocused(true);
+    onFocus?.(e);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setIsFocused(false);
+    onBlur?.(e);
+  };
+
   return (
     <>
       {label && (
-        <label className="inline-block mb-2 text-base font-bold align-baseline font">
+        <label
+          htmlFor={uuid.current}
+          className="inline-block mb-2 text-base font-bold align-baseline font"
+        >
           {label}
         </label>
       )}
       <div
+        onClick={onClickToFocus}
+        aria-hidden="true"
         className={classnames(
-          'input-wrapper',
+          "input-wrapper",
           SIZE_MAPS[size][key.wrapper],
           TYPE_MAPS[type][key.wrapper],
-          BORDERED_MAPS[bordered.toString()]
+          BORDERED_MAPS[bordered.toString()],
+          {
+            "ring-1 ring-primary-400": isFocused,
+            "opacity-40 border-gray-300 border cursor-not-allowed": disabled,
+          }
         )}
-        onClick={onClickToFocus}
-        onKeyDown={onClickToFocus}
-        role="textbox"
-        tabIndex={0}
       >
         {prefix && <Prefix size={size} icon={prefix} />}
         <input
+          id={uuid.current}
           ref={inputRef}
           placeholder={placeholder}
           type={htmlType}
           value={value}
           onChange={onChange}
           className={classnames(
-            'input',
+            "input",
             SIZE_MAPS[size][key.input],
             TYPE_MAPS[type][key.input]
           )}
+          disabled={disabled}
           onKeyPress={onKeyPress}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
         />
       </div>
     </>
