6af2ba6b75896f0eaa56d0a7c16cc582f638ea8d
Hao Huynh <haohuynh1101-hub>
2021-09-24 18:56:03 +0700

    update color

--- src/components/icons/BackIcon.tsx
+++ src/components/icons/BackIcon.tsx
@@ -18,6 +18,5 @@ export const BackIcon = () => {
         fill="currentColor"
       />
     </svg>
-
   );
 };
--- src/components/layout/head/index.tsx
+++ src/components/layout/head/index.tsx
@@ -4,29 +4,29 @@ import NextHead from 'next/head';
 
 import { APP_NAME } from '@/constants';
 type IProps = {
-    pageTitle?: string;
+  pageTitle?: string;
 };
 export const Head: React.FC<IProps> = ({ pageTitle }) => {
-    const title = pageTitle ? `${pageTitle} | ${APP_NAME}` : APP_NAME;
-    const description = 'Software with Powerful';
+  const title = pageTitle ? `${pageTitle} | ${APP_NAME}` : APP_NAME;
+  const description = 'Software with Powerful';
 
-    return (
-        <NextHead>
-            <title>{title}</title>
-            {/* <meta name="theme-color" content="#f37f26" /> */}
-            <meta name="description" content={description} />
-            <meta name="author" content="haohao" />
-            {/* Open Graph */}
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image:width" content="1200" />
-            <meta property="og:image:height" content="630" />
-            {/* Twitter Card */}
-            <meta name="twitter :card" content="summary" />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:creator" content="@haohao" />
-        </NextHead>
-    );
+  return (
+    <NextHead>
+      <title>{title}</title>
+      {/* <meta name="theme-color" content="#f37f26" /> */}
+      <meta name="description" content={description} />
+      <meta name="author" content="haohao" />
+      {/* Open Graph */}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      {/* Twitter Card */}
+      <meta name="twitter :card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:creator" content="@haohao" />
+    </NextHead>
+  );
 };
